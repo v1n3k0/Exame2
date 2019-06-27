088fa560e7be93dd1a31271ef350e6d31203bcc0
Vinicius Eiji Horiguchi <vinicius.horiguchi@verity.com.br>
2019-06-27 19:40:17 -0300

    conexao

---
+++ Eiji/Fontes/Exame/Exame.DAO/SqlCliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Exame.DAO
+{
+    public class SqlCliente
+    {
+
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        public SqlDataReader executar(SqlCommand comando)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataReader reader = null;
+                try
+                {
+                    connection.Open();
+                    reader = comando.ExecuteReader();
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+
+                return reader;
+            }
+        }
+    }       
+}
